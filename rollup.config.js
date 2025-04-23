4d695980c3b97e840b4a3727f8f434436fde5b1c
Kimberley Bonix <bonixkimberley@gmail.com>
2025-04-23 19:09:00 +0200

    new config

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import { terser } from "rollup-plugin-terser";
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
+import json from "@rollup/plugin-json";
 
 export default {
   input: "src/index.js",
@@ -10,6 +11,11 @@ export default {
     name: "Blanket",
 
     inlineDynamicImports: true,
+    sourcemap: true,
+    globals: {
+      "@supabase/supabase-js": "Supabase",
+    },
   },
-  plugins: [resolve(), commonjs(), terser()],
+  external: ["@supabase/supabase-js"],
+  plugins: [resolve(), commonjs(), json(), terser()],
 };
